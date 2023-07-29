c7b21a9b517010d899dc395ba757fe5fe495bcf6
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-07-30 00:25:00 +0300

    handleTagClick, handleSearch, view other profile(promptlar gelmiyor, düzeltilecek!)

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
+
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const getUserPosts = async () => {
+      const response = await fetch(`/api/users/${params?._id}/posts`);
+      const data = await response.json();
+      setUserPosts(data);
+    };
+    if (params?.id) getUserPosts();
+  }, [params.id]);
+
+  return (
+    //import Profile and send the props
+    <Profile
+      name={userName}
+      desc={`Greetings to the personalized profile page of ${userName}. Delve into ${userName}'s extraordinary prompts and let the force of their creativity ignite inspiration within you.`}
+      data={userPosts}
+    />
+  );
+};
+
+export default UserProfile;
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -17,21 +17,46 @@ const PromptCardList = ({data, handleTagClick}) => {
   )
 }
 const Feed = () => {
+  
+  const [allPosts, setAllPosts] = useState([])
+
+  // Search states
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([])
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
   const handleSearchChange = (e) => {
-    
+    setSearchText(e.target.value);
+    if (searchTimeout) clearTimeout(searchTimeout);
+    setSearchTimeout(
+      setTimeout(() => {
+        const results = allPosts.filter((post) =>
+          post.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setSearchedResults(results);
+      }
+      , 500)
+    );
   }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const results = allPosts.filter((post) =>
+      post.tag.toLowerCase().includes(tagName.toLowerCase())
+    );
+    setSearchedResults(results);
+  }
+
+  const fetchPosts = async () =>{
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+    setAllPosts(data);
+  }
+
   useEffect(()=>{
-    const fetchPosts = async () =>{
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-      setPosts(data);
-      
-    }
     fetchPosts();
   },[])
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -45,10 +70,15 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList
-        data={posts}
-        handleTagClick={()=>{}}
-      />
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
